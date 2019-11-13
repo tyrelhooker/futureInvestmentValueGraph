638db61030adb4be89d421fb227641057c1e65f6
TJ Hooker <tyrelhooker@gmail.com>
2019-11-13 00:48:02 -0500

    Initial commit

---
+++ futValGraph_Tkinter.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+window = Tk(className="window", )
+
+window.title("Investment Growth Chart")
+window.minsize(width=360, height=280)
+
+# label = tkinter.Label(window, text="Hello World!").pack()
+
+win = Canvas(window, width=320, height=240, bg='white')
+win.place(relx=0.5, rely=0.5, anchor=CENTER)
+# win.pack(anchor='center')
+
+label_y = 220
+label_x = 20
+label_values = 0
+year = 0
+year_y = 235
+year_x = 40
+
+for i in range(5):
+    win.create_text(label_x, label_y, text='{0}K'.format(label_values))
+
+    label_values += 2.5
+    label_y -= 50
+
+for years in range(11):
+    win.create_text(year_x, year_y, text='Y{0}'.format(years))
+
+    year_x += 25
+
+
+
+
+window.mainloop()
